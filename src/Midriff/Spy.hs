ec62d716b8ec3badd7f9fd946990583772dd1969
Eric Conlon <ejconlon@gmail.com>
2023-06-04 13:41:28 -0700

    update spy

--- src/Midriff/Spy.hs
+++ src/Midriff/Spy.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE MultiWayIf #-}
-
 module Midriff.Spy where
 
 import Control.Monad.IO.Class (liftIO)
@@ -9,29 +7,32 @@ import Data.Acquire (allocateAcquire)
 import Midiot.Osc (Packet)
 import Network.Socket qualified as NS
 
-oscSpyLocal :: Int -> Int -> Int -> IO ()
-oscSpyLocal inPort spyPort outPort = oscSpy (mkHp inPort) (mkHp spyPort) (mkHp outPort)
+oscSpyLocal :: Int -> Int -> IO ()
+oscSpyLocal spyPort destPort = oscSpy (mkHp spyPort) (mkHp destPort)
  where
   mkHp = HostPort (Just "127.0.0.1")
 
-oscSpy :: HostPort -> HostPort -> HostPort -> IO ()
-oscSpy inHost spyHost outHost = runResourceT $ do
-  inAddr <- liftIO (resolveAddr inHost)
-  outAddr <- liftIO (resolveAddr outHost)
+oscSpy :: HostPort -> HostPort -> IO ()
+oscSpy spyHost destHost = runResourceT $ do
+  destAddr <- liftIO (resolveAddr destHost)
   (_, srvConn) <- allocateAcquire (udpServerConn Nothing spyHost)
-  liftIO (oscSpyLoop inAddr outAddr srvConn)
+  liftIO (oscSpyLoop destAddr srvConn)
 
-oscSpyLoop :: NS.SockAddr -> NS.SockAddr -> Conn NS.SockAddr -> IO ()
-oscSpyLoop inAddr outAddr (Conn dec enc) = go
+oscSpyLoop :: NS.SockAddr -> Conn NS.SockAddr -> IO ()
+oscSpyLoop destAddr (Conn dec enc) = go Nothing
  where
-  go = do
+  go maySrcAddr = do
     (recvAddr, res) <- runDecoder dec
     case res of
       Left err -> print err
       Right (msg :: Packet) -> do
         print (recvAddr, msg)
-        if
-            | recvAddr == inAddr -> runEncoder enc outAddr msg
-            | recvAddr == outAddr -> runEncoder enc inAddr msg
-            | otherwise -> pure ()
-        go
+        if recvAddr == destAddr
+          then do
+            case maySrcAddr of
+              Just srcAddr -> runEncoder enc srcAddr msg
+              Nothing -> pure ()
+            go maySrcAddr
+          else do
+            runEncoder enc destAddr msg
+            go (Just recvAddr)
